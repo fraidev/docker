036a5b01d637ccec31fd66644cd6ce58b703731d
Felipe Cardozo <fraifelipe@gmail.com>
2021-12-20 17:40:17 -0300

    wip

---
+++ cron/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+)
+
+func main() {
+	fmt.Println("Started")
+	err := createSnapshot(false)
+	if err != nil {
+		log.Fatalln("erro full")
+	}
+	err = createSnapshot(true)
+	if err != nil {
+		log.Fatalln("erro roling")
+	}
+	fmt.Println("Started")
+}
+
+func createSnapshot(roling bool) error {
+	cmdToExecute := "tezos-node snapshot export --data-dir /var/run/tezos/node/data"
+
+	if roling {
+		cmdToExecute = cmdToExecute + " --rolling"
+	}
+
+	cmd := exec.Command(cmdToExecute)
+	stdout, err := cmd.Output()
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(stdout))
+
+	return nil
+}
